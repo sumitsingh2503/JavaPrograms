40208903616d10a63b6de4889ac1a38dff300125
sumitsingh2503 <sumit.singh1010@gmail.com>
2020-04-11 18:59:47 +0530

    Finonnaci.class

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -6,7 +6,7 @@ public class FibonacciSeries {
 		
 		int n1=0,n2=1,sum=0;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter how many numbers");
+		System.out.println("Enter how many numbers you want");
 		int var= sc.nextInt();
 		
 		System.out.println(n1+" "+n2);
